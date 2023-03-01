241ab399bda5fcbef7af9281452cc88192c05b99
canasmh <canasmh@yahoo.com>
2023-03-01 02:52:59 -0500

    Added download data button
    Screenshot shows two buttons: a 'view data' button and a 'download data' button. So I added the download data button. I was hesitant because there was no mention of it in commented code nor in the readme, but instructions specifically say to reproduce what is in the screenshot

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -68,6 +68,13 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => console.log('downloading data (???)')}
+        >
+          Download the Data
+        </Button>
       </div>
 
       <div className="middle-section">
